385d5b5e98acdfeaa100f31591c1c1990a6ac05e
shuklasid19 <shuklasid19@gmail.com>
2022-09-24 01:36:36 +0530

    changes done

--- src/qunix/utility.py
+++ src/qunix/utility.py
@@ -1,5 +1,12 @@
+from re import sub
+
+
 def addition(a,b):
     return a+b
 
 def subtraction(a,b):
     return a-b
+
+print("addition is =", addition(23,54))
+
+print("subtraction is =", subtraction(54, 34))
